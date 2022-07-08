e88c635e7f701f9ca29b30799c5913a846c35766
sw9504 <sladewilson9504@gmail.com>
2022-07-07 21:20:56 -0300

    Add working numbers, clear and delete btn

--- js/app.js
+++ js/app.js
@@ -17,3 +17,22 @@ function divide (a,b) {
 function operate (op,a,b) {
 
 }
+
+function display (e){
+    const btn = e.target.attributes['key-op'].value;
+    const p = document.querySelector("p");
+
+    if (btn === 'clear') p.textContent = "";
+
+    else if (btn === 'delete') 
+        p.textContent = p.textContent.slice(0,p.textContent.length-1);
+
+    else if (parseInt(btn) >= 0 || parseInt(btn) <= 9)
+        p.textContent += btn;
+    
+}
+
+const keys = Array.from(document.querySelectorAll(".keys"));
+keys.forEach(key => {
+    key.addEventListener("click", display);
+});
